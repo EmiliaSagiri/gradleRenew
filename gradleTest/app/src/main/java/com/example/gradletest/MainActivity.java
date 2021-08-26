a9d34b39c60484bfac57f224a205e199ee121ce9
Le <uidq3372@v01.net>
2021-08-26 10:53:20 +0800

    实现了扒网站，以及过滤出链接并下载

--- gradleTest/app/src/main/java/com/example/gradletest/MainActivity.java
+++ gradleTest/app/src/main/java/com/example/gradletest/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-
+public static final String  TAG = "sb";
+static ArrayList<String> sum = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-             String content = Http.get("https://services.gradle.org/distributions/");
+                String version = "7.2";
+                String pattern = "/distributions/gradle-"+ version + "-all.zip.sha256";
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(File.read("/vr/test/1.txt"));
+                while (m.find()) {
+                    sum.add(m.group());
+                }
+                String http =" https://services.gradle.org/"+sum.get(0);
+                Log.i(TAG, http);
+                Http.download(http,"vr/test/2.txt");
             }
         }).start();
     }
